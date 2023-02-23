08ba3f0d1490a37adab3d0c5a226c876d3a01cce
Anshu Ashish <anshuashishjha@gmail.com>
2023-02-24 00:58:57 +0530

    Initial

---
+++ spring-boot-example/src/main/java/com/aashish/concept/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.aashish.concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
